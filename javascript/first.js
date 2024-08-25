5247b5c3573a6357396616eef4f2a51cd55a4f58
SUKUNA-001 <manik.kumar1850@gmail.com>
2024-08-25 15:26:19 +0530

    Create first.js

---
+++ javascript/first.js
@@ -0,0 +1,112 @@
+// console.log("manik");
+// age =23;
+// console.log(age);
+// isfollow = true;
+// console.log(isfollow)
+// a=null;
+// console.log(a)
+
+
+//arthematic op//
+// let a= 4;
+// let b = 2;
+// console.log("a+b=", a+b);
+// console.log("a-b=", a+b);
+// console.log("a*b=", a*b);
+// console.log("a/b=", a/b);
+// console.log("a%b=", a%b);
+// console.log("a**b=", a**b);
+
+/**unary operator*/
+
+//  let a= 4;
+//  let b = 2;
+
+
+//  //console.log("a=",a)
+//  console.log("++a=",++a)
+
+
+/** assignment operator */
+// let a = 5;
+// let b = 2;
+
+// a -= 4; // a = a - 4
+// console.log("a=",a)
+
+//console.log("a==b",a==b)
+
+/**logical operator */
+// let a=6;
+// let b=5;
+
+// let cond1 = a>b;
+// let cond2 = a===6;
+// console.log("cond1 && cond2=",cond1 && cond2)
+// console.log("cond1 || cond2=",a>b || a===5 )
+// console.log("!(6>5)=", !(a>b))
+
+/** CONDITIONAL STATEMENT */
+// let age = 15;
+
+// if (age>18) {
+//     console.log("yuo can vote");
+
+// }
+// if (age<18){
+//     console.log("u can't vote");
+
+// }
+
+/** IF STATEMENT */
+
+// let mode = "dark";
+// if (mode === "dark"){
+//     color = "black"
+// }
+
+// if (mode === "light"){
+//     color = white;
+// }
+
+// console.log(color);
+
+/** PRACTICE */
+
+
+// let age = prompt("enter age")
+
+// if (age>=18){
+//     console.log("vote")
+// }
+
+// else{
+//     console.log("not vote")
+// }
+
+/** PRACTICE */
+// let age = 24;
+// let result = age >=18 ? "adult" : "not adult";
+// console.log(result);
+
+/**PRACTICE QUES */
+
+//  let num = prompt("enter number");
+//  if (num>=90){
+//      console.log("grade A")
+//  }
+//  else if (num>=70 && num<=89){
+//      console.log("grade B")
+//  }
+//  else if (num>=60 && num>=69){
+//      console.log("grade C")
+//  }
+//  else if (num>=50 && num<=59){
+//      console.log("grade D")
+//  }
+//  else{
+//      console.log("grade F")  
+//  }
+
+
+
